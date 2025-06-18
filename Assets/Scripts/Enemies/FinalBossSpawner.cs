ea03434ccf3489f1ca0b37c1424ea6e01a77e8ee
JakeStudio21 <rohyy0126@gmail.com>
2025-06-18 19:28:09 +0900

    boss몬스터 스폰딜레이 구현

--- Assets/Scripts/Enemies/FinalBossSpawner.cs
+++ Assets/Scripts/Enemies/FinalBossSpawner.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using System.Collections; // ← 꼭 추가!
+
 
 public class FinalBossSpawner : MonoBehaviour
 {
@@ -15,21 +17,27 @@ public class FinalBossSpawner : MonoBehaviour
     }
 
     // 보스가 죽을 때마다 호출
+
     public void OnBossDeath(string bossId, Vector3 deathPosition)
+{
+    if (bossId == "FinalBossA")
     {
-        if (bossId == "FinalBossA")
-        {
-            SpawnBoss(finalBossBPrefab, deathPosition + new Vector3(1, 0, 0)); // 약간 옆에 스폰
-        }
-        else if (bossId == "FinalBossB")
-        {
-            SpawnBoss(finalBossCPrefab, deathPosition + new Vector3(1, 0, 0));
-        }
-        else if (bossId == "FinalBossC")
-        {
-            // 마지막 보스 처치 후 연출(문 열기, 엔딩 등)
-            Debug.Log("모든 보스 처치! 엔딩 연출 등");
-        }
+        StartCoroutine(SpawnBossWithDelay(finalBossBPrefab, deathPosition + new Vector3(1, 0, 0), 1.5f)); // 딜레이 적용!
+    }
+    else if (bossId == "FinalBossB")
+    {
+        StartCoroutine(SpawnBossWithDelay(finalBossCPrefab, deathPosition + new Vector3(1, 0, 0), 1.5f)); // 딜레이 적용!
+    }
+    else if (bossId == "FinalBossC")
+    {
+        Debug.Log("모든 보스 처치! 엔딩 연출 등");
+    }
+}
+
+    private IEnumerator SpawnBossWithDelay(GameObject bossPrefab, Vector3 pos, float delay) // ★
+    {
+        yield return new WaitForSeconds(delay);
+        SpawnBoss(bossPrefab, pos);
     }
 
     private void SpawnBoss(GameObject bossPrefab, Vector3 pos)
@@ -37,11 +45,23 @@ public class FinalBossSpawner : MonoBehaviour
         if (bossPrefab == null) return;
         currentBoss = Instantiate(bossPrefab, pos, Quaternion.identity);
 
-        // EnemyHealth에 스포너 연결
         EnemyHealth eh = currentBoss.GetComponent<EnemyHealth>();
         if (eh != null)
         {
             eh.bossSpawner = this;
         }
     }
+
+    // private void SpawnBoss(GameObject bossPrefab, Vector3 pos)
+    // {
+    //     if (bossPrefab == null) return;
+    //     currentBoss = Instantiate(bossPrefab, pos, Quaternion.identity);
+
+    //     // EnemyHealth에 스포너 연결
+    //     EnemyHealth eh = currentBoss.GetComponent<EnemyHealth>();
+    //     if (eh != null)
+    //     {
+    //         eh.bossSpawner = this;
+    //     }
+    // }
 }
